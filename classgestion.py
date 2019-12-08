d96d7555fea4570cbbd149d42b2d1f705a18e934
Abdelkerim Mahamat Zeni <58027608+abdel-137@users.noreply.github.com>
2019-12-08 14:38:20 +0100

    classtravailleur

---
+++ classgestion.py
@@ -0,0 +1,29 @@
+ #coding:utf-8
+ 
+
+ class travailleur:
+    def __init__(self, nom , ide)
+        self.nom = nom
+        self.ide = ide
+         
+    def travailleur(self): 
+         print("l'agent{}, matricule{} travaille dans cette banque et il est{}:".format(self.nom, self.ide, self.poste)
+         print("\t$%$%$%$%$%$%$%$%$%")
+     
+ class guichetier....
+
+
+
+    def retrait(montant, solde = som)
+        print("opération du retrait :)")
+        print("\t%$%$%$%$%$%$%$%$%$%)
+        montant = input("Entrez le montant à retirer:")
+        montant = int(montant)
+        if montant >= solde:
+            print("Désoler")
+        else:
+        solde -= montant
+        print("\t $%$%$%$%$%$%$%$%")
+        print("retrait effectué avec succées")
+        print("\t $%$%$%$%$%$%$%$%$% ")
+
